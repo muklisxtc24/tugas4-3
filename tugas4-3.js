2a937374e1cd8287f186b7e47205cfab821b951c
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-07-29 20:38:27 +0700

    Add files via upload

---
+++ tugas4-3.js
@@ -0,0 +1,3 @@
+var looping = ['Santai', 'Berkualitas', 'I Love Coding', 'Berkulitas', 'Santai', 'Berkualitas', 'I Love Coding', 'Berkualitas','Santai', 'Berkualitas', 'I Love Coding', 'Berkualitas','Santai', 'Berkualitas', 'I Love Coding', 'Berkualitas','Santai', 'Berkualitas', 'I Love Coding', 'Berkualitas']
+for (i = 0; i < looping.length; i++) 
+    document.write(i + 1 + ' ' + '-' + ' ' +  looping[i] + '<br>')
